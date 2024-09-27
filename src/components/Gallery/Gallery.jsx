4b45194f1074a5ab442d85a21157df8e45e0c4fb
imyroslav <imyroslav@ukr.net>
2024-09-27 23:36:39 +0700

    updated-gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,5 +1,6 @@
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from "react-image-gallery";
+import css from "./Gallery.module.css";
 
 
 export default function Gallery() {
@@ -17,10 +18,28 @@ const images = [
     original: "https://picsum.photos/id/1019/1000/600/",
     thumbnail: "https://picsum.photos/id/1019/250/150/",
     },
+  {
+    original: "https://picsum.photos/id/1020/1000/600/",
+    thumbnail: "https://picsum.photos/id/1020/250/150/",
+    },
+  {
+    original: "https://picsum.photos/id/1021/1000/600/",
+    thumbnail: "https://picsum.photos/id/1021/250/150/",
+    },
+  {
+    original: "https://picsum.photos/id/1022/1000/600/",
+    thumbnail: "https://picsum.photos/id/1022/250/150/",
+  },
 ];
     
     return (
-        <ImageGallery items={images} />
+        <>
+        
+            <p className={css.object}>НАШИ ОБЪЕКТЫ</p>
+            <ImageGallery items={images} />
+            
+        </>
+        
     )    
 
 
